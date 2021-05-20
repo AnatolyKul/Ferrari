996edb52dcc9b5056690e9fd6c2ab376ff9641cf
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-20 16:45:42 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Automobile
+    {
+        public Automobile(string v1, string v2, string v3, string v4)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+            V4 = v4;
+        }
+
+        public string V1 { get; }
+        public string V2 { get; }
+        public string V3 { get; }
+        public string V4 { get; }
+        internal void name (string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void color(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void model(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void speed(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void move (Automobile v)
+        {
+            Console.WriteLine("Ferrari move");
+        }
+        internal void stop (Automobile v)
+        {
+            Console.WriteLine("Ferrari stop");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobile Ferrari = new Automobile("Ferrari", "Red", "EVA 599XX", "530 km/h");
+            Ferrari.name("Ferrari");
+            Ferrari.color("Red");
+            Ferrari.model("EVA 599XX");
+            Ferrari.speed("530 km/h");
+            Ferrari.move(Ferrari);
+            Ferrari.stop(Ferrari);
+        }
+    }
+}
